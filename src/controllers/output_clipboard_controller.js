6e8158654bfad6ffd5c692d64957867c3225fe93
Mathew Button <mat@pushbutton.xyz>
2022-08-20 22:27:38 +1000

    Add selected logic to template and add clipboard message
    I forgot to split up the commits when I made all the indentation changes
    ahh welll

--- src/controllers/output_clipboard_controller.js
+++ src/controllers/output_clipboard_controller.js
@@ -1,13 +1,15 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["input"]
-
-  connect() {
-
-  }
+  static targets = ["input", "message"]
 
   copyToClipboard() {
     navigator.clipboard.writeText(this.inputTarget.value)
+    this.messageTarget.classList.add("opacity-100");
+    this.messageTarget.classList.add("instant-transition");
+    setTimeout(() => {
+      this.messageTarget.classList.remove("instant-transition");
+      this.messageTarget.classList.remove("opacity-100");
+    },100)
   }
 }
