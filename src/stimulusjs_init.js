a4cc737d866467b89bc21cc22926167599ccbd20
Mathew Button <mat@pushbutton.xyz>
2022-08-20 22:28:55 +1000

    Clean up unused emoji picker controller

--- src/stimulusjs_init.js
+++ src/stimulusjs_init.js
@@ -1,11 +1,9 @@
 // src/application.js
 import { Application } from "@hotwired/stimulus"
 
-import EmojiPickerController from "./controllers/emoji_picker_controller"
 import ThemeGeneratorController from "./controllers/theme_generator_controller"
 import OutputClipboardController from "./controllers/output_clipboard_controller"
 
 window.Stimulus = Application.start();
-Stimulus.register("emoji-picker", EmojiPickerController);
 Stimulus.register("theme-generator", ThemeGeneratorController);
 Stimulus.register("output-clipboard", OutputClipboardController);
