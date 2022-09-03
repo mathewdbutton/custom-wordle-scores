0aa1b82090c7b6943d15268f6c9d8b2a460939ba
Mathew Button <mat@pushbutton.xyz>
2022-09-03 20:32:57 +1000

    Move emoji list to src

--- src/controllers/emoji_picker_controller.js
+++ src/controllers/emoji_picker_controller.js
@@ -1,5 +1,5 @@
 import { Controller } from "@hotwired/stimulus"
-import emojis from "../../filtered_emoji_list.json"
+import emojis from "../filtered_emoji_list.json"
 
 export default class extends Controller {
   static targets = ["input", "result", "results"]
